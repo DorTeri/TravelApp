d797b10967b4d525a5a5099bad801fa3a3639d17
DorTeri <dortayari@gmail.com>
2023-11-19 09:26:45 +0200

    Added GetApp section

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,13 +4,14 @@ type ButtonProps = {
     type: 'button' | 'submit';
     title: string;
     icon?: string;
-    variant: 'btn_dark_green' | 'btn_green' | "btn_white_text";
+    variant: 'btn_dark_green' | 'btn_green' | "btn_white_text" | "btn_dark_green_outline" | "btn_white";
+    full?: boolean
 }
 
-const Button = ({type , title, icon , variant}: ButtonProps) => {
+const Button = ({type , title, icon , variant , full}: ButtonProps) => {
   return (
     <button type={type} 
-    className={`flexCenter gap-3 rounded-full border ${variant}`}>
+    className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}>
         {icon && <Image src={icon} alt={title} width={24} height={24} />}
         <label className='bold-16 whitespace-nowrap'>{title}</label>
     </button>
